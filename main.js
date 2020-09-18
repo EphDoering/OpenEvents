4c2790dc9ba6298331dc371beab613b0c4b5f64f
frederick_efd <frederick.doering@nordson.com>
2020-09-18 11:12:55 -0400

    Got minimal working bulk patch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(  function(){
+let handleClientLoad=(  function(){
   var GoogleAuth;
   var SCOPE = 'https://www.googleapis.com/auth/calendar';
   function handleClientLoad() {
@@ -64,7 +64,7 @@
   function revokeAccess() {
     GoogleAuth.disconnect();
   }
-
+var colorSelect=null
   function setSigninStatus(isSignedIn) {
     var user = GoogleAuth.currentUser.get();
     var isAuthorized = user.hasGrantedScopes(SCOPE);
@@ -74,6 +74,27 @@
       $('#auth-status').html('You are currently signed in and have granted ' +
           'access to this app.');
       populateCalendarList();
+      gapi.client.calendar.colors.get()
+	.then(resp=>{
+        colorSelect=document.createElement('select')
+        let op=document.createElement('option')
+        op.innerHTML='unchanged'
+        colorSelect.appendChild(op)
+        for(let color in resp.result.event){
+            let op=document.createElement('option')
+            op.style.color=resp.result.event[color].background
+            op.value=color
+            op.innerHTML='&#x2B24;'
+            colorSelect.appendChild(op)
+        }
+        colorSelect.addEventListener('change',updateColorOfSelectOnChange)
+        for(let input of document.querySelectorAll('input[name=colorId]')){
+          var newSel=colorSelect.cloneNode(true)
+          newSel.name=input.name
+          newSel.addEventListener('change',updateColorOfSelectOnChange)
+          input.replaceWith(newSel)
+        }
+    })
     } else {
       $('#sign-in-or-out-button').html('Sign In/Authorize');
       $('#revoke-access-button').css('display', 'none');
@@ -101,37 +122,89 @@
       }
     });
   }
+  var searchResults=null;
 document.forms.search.addEventListener('submit',function(e){
-  e.preventDefault();
-  var obj={}
-  var types={};
-  $(document.forms.search).serializeArray().forEach(function(item){
-    if(item.value){
-      switch(discoveryAPI.resources.events.methods.list.parameters[item.name].format){
-        case "date-time":
-          obj[item.name]=(new Date(item.value)).toISOString();
-          break;
-        default:
-          types[discoveryAPI.resources.events.methods.list.parameters[item.name].format]=true;
-          obj[item.name]=item.value;
-      }
-    }
-  })
-  console.log(obj);
-  console.log(types);
-  gapi.client.calendar.events.list(obj).then(console.log,console.error);
+  var obj=prep_form_to_send(e,discoveryAPI.resources.events.methods.list.parameters)
+  gapi.client.calendar.events.list(obj).then(gotSearchResults.bind(null,obj),console.error);
   return false;
 })
 
 
+var patch_body=null
+document.forms.patch.addEventListener('submit',function(e){
+  if(remaining_to_patch.length){
+    e.preventDefault();
+    console.error('attempted patch while already patching')
+    return false;
+  }
+  patch_body=prep_form_to_send(e,discoveryAPI.schemas.Event.properties)
+  remaining_to_patch=searchResults.responce.items.slice()
+  progress_bar.max=remaining_to_patch.length
+  progress_bar.value=+progress_bar.max-remaining_to_patch.length
+  document.body.appendChild(progress_bar)
+  
+  doPatch()
+
+  return false;
+})
+var progress_bar=document.createElement('progress')
+var remaining_to_patch=[]
+function doPatch(){
+    let next=remaining_to_patch.pop()
+    progress_bar.value=+progress_bar.max-remaining_to_patch.length
+    if(next){
+        return gapi.client.calendar.events.patch({
+          calendarId:searchResults.calendarId,
+          eventId:next.id
+          },patch_body)
+        .then(doPatch,console.error)
+    }
+    progress_bar.remove()
+}
+
+function prep_form_to_send(e,fields){
+  e.preventDefault();
+  var obj={}
+  var types={};
+  $(e.target).serializeArray().forEach(function(item){
+    if(item.value){
+      let format=fields[item.name].format;
+      switch(format){
+        case "date-time":
+          obj[item.name]=(new Date(item.value)).toISOString();
+          break;
+        case undefined:
+          obj[item.name]=item.value;
+          break;
+        default:
+          types[format]=true;
+          obj[item.name]=item.value;
+      }
+    }
+  })
+  if(types.length){
+    console.warn(types)
+  }
+  return obj
+}
+
+var searchResults=null;
+function gotSearchResults(searchobj,results){
+    searchResults=searchobj
+    searchResults.responce=results.result;
+    document.getElementById('found_count').innerText=searchResults.responce.items.length
+}
+
+
 var initializeForms=function(){
 	var addStuff=createFormTable(discoveryAPI.resources.events.methods.list.parameters,listEventParams);
-	addStuff.firstChild.removeChild(addStuff.firstChild.firstChild);
-	document.forms.search.replaceChild(addStuff,document.forms.search.calendarId.nextSibling);
+	document.forms.search.replaceChild(addStuff,document.forms.search.calendarId.nextElementSibling);
+	var edit=createFormTable(discoveryAPI.schemas.Event.properties,editEventParams)
+	document.forms.patch.replaceChild(edit,document.forms.patch.firstElementChild)
 }
 
 var listEventParams={
-  calendarId:"To Be Removed",
+  calendarId:"",
   orderBy:"Order By",
   q:"Search Text",
   showDeleted:"Include Deleted",
@@ -141,34 +214,57 @@ var listEventParams={
   updatedMin:"Modified Since"
 }
 
+var editEventParams={
+  id:"",
+  updated:"",
+  created:"",
+  privateCopy:"",
+  locked:"",
+  recurringEventId:"",
+  kind:"",
+  etag:"",
+  organizer:"",
+  iCalUID:"",
+  originalStartTime:"",
+  creator:"",
+  start:"",
+  end:"",
+  summary:"Summary",
+  description:"Description",
+  location:"Location",
+  colorId:"Color",
+  visibility:"Visibility"
+}
+
 var autoPrefix=0;
 var createFormTable=function(parameters,labels,prefix){
   prefix=prefix|| "p"+(autoPrefix++);
   var mainDiv=document.createElement('div');
   var table=document.createElement('table');
   for(var name in labels){
-    if(parameters[name]){
-      table.appendChild(createRow(parameters[name],labels[name],prefix));
+    if(parameters[name] && labels[name]){
+      table.appendChild(createRow(name,parameters[name],labels[name],prefix));
     }
   }
   mainDiv.appendChild(table);
   mainDiv.appendChild(document.createElement('label'));
-  mainDiv.lastChild.innerText="Show All";
-  mainDiv.lastChild.HTMLfor=prefix+"ShowAllCheckbox";
+  mainDiv.lastChild.innerText="Show Advanaced Parameters";
+  mainDiv.lastChild.htmlFor=prefix+"ShowAllCheckbox";
   mainDiv.appendChild(document.createElement('input'));
   mainDiv.lastChild.type='checkbox';
   mainDiv.lastChild.className='show-hide-next';
+  mainDiv.lastChild.id=prefix+"ShowAllCheckbox";
   table=document.createElement('table');
   for(var name in parameters){
-    if(!labels[name]){
-      table.appendChild(createRow(parameters[name],name,prefix));
+    if(typeof labels[name] == 'undefined'){
+      table.appendChild(createRow(name, parameters[name],name,prefix));
     }
   }
   mainDiv.appendChild(table);
   return mainDiv;
 }
 
-var createRow=function(parameter,label,prefix){
+var createRow=function(name, parameter,label,prefix){
   var row=document.createElement('tr');
   var cell=document.createElement('td');
   cell.appendChild(document.createElement('input'));
@@ -215,6 +311,12 @@ var createRow=function(parameter,label,prefix){
   return row;
 }
 
+function updateColorOfSelectOnChange(e){
+  let sel=e.target
+  sel.style.color=sel.selectedOptions[0].style.color
+}
 
 
+
+return handleClientLoad;
 })();
